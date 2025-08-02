e708702f6a5891b4da53503dd7647b2085bc2c57
JackiyWong <1903915989@qq.com>
2025-08-02 19:16:16 +0800

    后端初始化

---
+++ aicodebackend/src/main/java/com/jackiy/aicode/AicodebackendApplication.java
@@ -0,0 +1,13 @@
+package com.jackiy.aicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicodebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicodebackendApplication.class, args);
+    }
+
+}
