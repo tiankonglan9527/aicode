a32c0957559854b1e254359bc35ecac9d3da334e
JackiyWong <1903915989@qq.com>
2025-08-03 02:03:15 +0800

    other

---
+++ aicodebackend/src/main/java/com/jackiy/aicode/service/UserService.java
@@ -0,0 +1,88 @@
+package com.jackiy.aicode.service;
+
+import com.jackiy.aicode.model.dto.user.UserQueryRequest;
+import com.jackiy.aicode.model.entity.User;
+import com.jackiy.aicode.model.vo.LoginUserVO;
+import com.jackiy.aicode.model.vo.UserVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+ * 用户 服务层。
+ *
+ * @author <a href="https://github.com/tiankonglan9527">天空岚</a>
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    // 加密
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request 请求域
+     * @return 当前登录用户
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request 请求域
+     * @return 注销是否成功
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获取脱敏的用户信息
+     *
+     * @param user 用户
+     * @return 脱敏后的用户信息
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取脱敏的用户信息列表
+     *
+     * @param userList 用户列表
+     * @return 脱敏后的用户信息列表
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 分页查询用户信息
+     *
+     * @param userQueryRequest 查询条件
+     * @return 满足条件的用户信息
+     */
+    QueryWrapper QueryWrapper(UserQueryRequest userQueryRequest);
+}
