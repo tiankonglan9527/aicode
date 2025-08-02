efd9e2bcae6c27375c8777e595f473e0b0ba5020
JackiyWong <1903915989@qq.com>
2025-08-02 20:49:52 +0800

    后端初始化完成：

--- aicodebackend/src/main/java/com/jackiy/aicode/AicodebackendApplication.java
+++ aicodebackend/src/main/java/com/jackiy/aicode/AicodebackendApplication.java
@@ -2,7 +2,10 @@ package com.jackiy.aicode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+// 开启AspectJ自动代理的支持
+@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class AicodebackendApplication {
 
