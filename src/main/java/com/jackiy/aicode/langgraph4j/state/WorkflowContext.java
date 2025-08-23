014bc29f79bb17bcbaf05db3e8a632c8100371ef
JackiyWong <1903915989@qq.com>
2025-08-23 21:58:00 +0800

    LangGraph4j重构完成

--- src/main/java/com/jackiy/aicode/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/jackiy/aicode/langgraph4j/state/WorkflowContext.java
@@ -1,6 +1,7 @@
 package com.jackiy.aicode.langgraph4j.state;
 
 import com.jackiy.aicode.langgraph4j.model.ImageResource;
+import com.jackiy.aicode.langgraph4j.model.QualityResult;
 import com.jackiy.aicode.model.enums.CodeGenTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -66,6 +67,11 @@ public class WorkflowContext implements Serializable {
      * 构建成功的目录
      */
     private String buildResultDir;
+
+    /**
+     * 质量检查结果
+     */
+    private QualityResult qualityResult;
 
     /**
      * 错误信息
